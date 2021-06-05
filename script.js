88b0dc1ada5ede35840baebb07aa67e4f9ec3800
tiru18324 <71921628+tiru18324@users.noreply.github.com>
2021-06-05 17:36:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
-function compute()
+function computeInterest()
 {
-    var principal = document.getElementById("principal").value;
-    var rate = document.getElementById("rate").value;
-    var years = document.getElementById("years").value;
-    var year = new Date().getFullYear()+parseInt(years);
-    var interest = principal * years * rate /100;
-	
+     var principal = document.getElementById("principal").value;
 
-    function updateRate() 
-{
-    var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText=rateval;
+     //Check if principal  is 0 or negative
+    if(principal < 1){
+        alert("Please enter a positive value");
+        document.getElementById("principal").focus();
+        return false;
+    }
+     var rate = document.getElementById("rate").value;
+     var years = document.getElementById("years").value;
+     //Calculate Interest
+     var interest = principal * years * rate /100;
+     var year = new Date().getFullYear()+parseInt(years) ;
+     var amount = principal + interest
+     //show the result
+     document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
 
 }
- document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>"
-   
 
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval +"%";
 }
